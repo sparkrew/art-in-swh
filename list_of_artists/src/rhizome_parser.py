e8c791f82aa4061b92be59bdb3804343dab3bd1a
bbaudry <baudry@kth.se>
2025-07-15 15:51:26 -0400

    add list of artists on artblocks

--- list_of_artists/src/rhizome_parser.py
+++ list_of_artists/src/rhizome_parser.py
@@ -15,10 +15,10 @@ if response.ok:
     headers = soup.find_all('li')
     for header in headers:
     	# remove the number of occurences that is in () after the name
-    	parts=header.text.split('(')
+        parts=header.text.split('(')
     	# save the artist name in file
-    	artistslist.write(parts[0]+'\n')
-    artistslist.close()
+        artistslist.write(parts[0]+'\n')
+        artistslist.close()
 else:
     print ("Boo! {}".format(response.status_code))
     print (response.text)
