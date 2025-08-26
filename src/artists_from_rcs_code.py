a985752fd32963131ce7de4771db6537b42cf0b8
Roxana Peña Mendieta <97145007+Selleen@users.noreply.github.com>
2025-08-26 11:39:02 -0400

    Join the files

--- src/artists_from_rcs_code.py
+++ src/artists_from_rcs_code.py
@@ -5,13 +5,13 @@ import pandas as pd
 import re
 
 
-def save_and_print(artist_names, i):
+def save_and_print(artist_names, file_name):
     # Save in .txt file and print the results
-    with open(f"artists_from_rcs_case_{i}.txt", "w", encoding="utf-8") as f:
+    with open(f"{file_name}.txt", "w", encoding="utf-8") as f:
         for artist in sorted(artist_names):
             f.write(artist + "\n")
 
-    print(f"{len(artist_names)} possible artist found and saved in 'artists_from_rcs_case_{i}.txt'")
+    print(f"{len(artist_names)} possible artist found and saved in '{file_name}.txt'")
     
 def get_artists_case_1(soup):
     # Use web-scrapping only: Split the lines and look for the line with only the 'by' text, then the next line has the 
@@ -28,7 +28,6 @@ def get_artists_case_1(soup):
             if i + 1 < len(lines):
                 next_line = lines[i + 1].strip()
                 parts = re.split(r",| and ", next_line)
-                print(f"parts = {parts}")
                 # Remove spaces
                 artist_list.extend([p.strip() for p in parts if p.strip()])
             i += 2
@@ -57,6 +56,18 @@ def get_artist_case_2(nlp, soup):
 
     return artist_names
 
+def union_method(result1, result2):
+    output_file_name = "artists_from_rcs.txt"
+
+    # Join sets
+    artist_names = set(result1) | set(result2)
+
+    # Save
+    with open(output_file_name, "w", encoding="utf-8") as f:
+        for name in sorted(artist_names):
+            f.write(name + "\n")
+
+    print(f"Completed join. Generated file: {output_file_name}")
 
 if __name__ == "__main__":
     nlp = spacy.load("en_core_web_sm")
@@ -64,10 +75,15 @@ if __name__ == "__main__":
     response = requests.get(url)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, "html.parser")
+    #file_list = ["artists_from_rcs_case_1", "artists_from_rcs_case_2"]
     
     # Case 1
     result1 = get_artists_case_1(soup)
-    save_and_print(result1, 1)
+    #save_and_print(result1, file_list[0])
+    
     # Case 2
     result2 = get_artist_case_2(nlp, soup)
-    save_and_print(result2, 2)
+    #save_and_print(result2, file_list[1])
+    
+    # Join files
+    union_method(result1, result2)
