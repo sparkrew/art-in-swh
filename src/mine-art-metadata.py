10c6cb70e2d3e97256c53e09c746d7f786bb42da
bbaudry <baudry@kth.se>
2025-11-19 17:27:51 -0500

    added location of user, bio and languages for bio and readme

--- src/mine-art-metadata.py
+++ src/mine-art-metadata.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import pycld2 as cld2
 from dotenv import load_dotenv # type: ignore
 
 load_dotenv()
@@ -9,12 +10,26 @@ headers = {"Authorization": "Bearer "+os.getenv("API_KEY")}
 
 def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
     
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    request = requests.post(
+        'https://api.github.com/graphql', 
+        json={'query': query}, 
+        headers=headers)
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
+    
+def get_user_location(username):
+    request = requests.post(
+        'https://api.github.com/graphql', 
+        json={"query": queryforuser, "variables": {"login": username}},
+        headers=headers)
+    if request.status_code == 200:
+        data = request.json()
+        return data["data"]["user"]
+        
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
         
 # The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
 queryrate = """
@@ -44,7 +59,7 @@ for i in data:
 # The outer GraphQL braces must be escaped with double braces {{ ... }} so that Python doesn’t treat them as placeholders.
 # Inside the query, the query: field expects a quoted string, so you wrap {s} in ".
 # The idea of search comes from https://til.simonwillison.net/github/bulk-repo-github-graphql
-query2 = f"""
+queryforrepositories = f"""
 {{
   search(type: REPOSITORY, query: "{search_query}", first: 100) {{
     nodes {{
@@ -85,13 +100,24 @@ query2 = f"""
 }}
 """
 
-# result_rate = run_query(queryrate)
+queryforuser = """
+query getUserLocation($login: String!) {
+  user(login: $login) {
+    login
+    location
+    createdAt
+    bio
+  }
+}
+"""
+
+ratebefore = run_query(queryrate)
 # remaining_rate_limit = result_rate["data"]["rateLimit"]["remaining"] # Drill down the dictionary
 # print("Remaining rate limit - {}".format(remaining_rate_limit))
 
-result = run_query(query2) # Execute the query
-rel = result["data"]["search"]["nodes"]
-for repo in rel:
+result = run_query(queryforrepositories)
+allrepos = result["data"]["search"]["nodes"]
+for repo in allrepos:
     name=repo["nameWithOwner"]
     print(name)
     print("+++++++++++++++++++++++++++++++++++++")
@@ -105,11 +131,26 @@ for repo in rel:
     if len(contributors)!= 0:
       main_contributor = max(set(contributors), key=contributors.count)
       print(f"Main contributor: {main_contributor}")
+      loc=get_user_location(main_contributor)
+      if len(loc)!=0:
+          print(f"Location: {loc["location"]}")
+          print(f"Bio: {loc["bio"]}")
+          isReliable, textBytesFound, details = cld2.detect(loc["bio"])
+          if isReliable : print(f"Languages of bio: {str(details)}")
+          print(f"createdAt: {loc["createdAt"]}")
     else:
       print("Main contributor: no contributor is listed")
+    # file_count = len(repo["object(expression: \"HEAD:\")"]["entries"])
+    # print(f"Number of files in root: {file_count}")
     # file_count = len(repo["object(expression: \"HEAD:\")"]["entries"])
     # print("Number of files at root: "+file_count)
     readme = repo["object"]["text"] if repo["object"] else None
     print(readme)
+    isReliable, textBytesFound, details = cld2.detect(readme)
+    if isReliable : print(f"Languages of readme: {str(details)}")
     print("+++++++++++++++++++++++++++++++++++++")
 
+
+rateafter = run_query(queryrate)
+total_hits = ratebefore["data"]["rateLimit"]["remaining"] - rateafter["data"]["rateLimit"]["remaining"]# Drill down the dictionary
+print("Total requests - {}".format(total_hits)+". Was there before {}".format(ratebefore["data"]["rateLimit"]["remaining"])+", and now {}".format(rateafter["data"]["rateLimit"]["remaining"]))
